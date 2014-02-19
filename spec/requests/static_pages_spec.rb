72bb81d330dc6c81a50871d1535a6418f6cb0908
David Williams <davew1982@gmail.com>
2014-02-19 16:03:50 -0500

    Added more tests and a few new views

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,12 @@ describe "Static Pages" do
       expect(page).to have_content('Help')
     end
   end
+  describe "About page" do 
+    it "should have the content 'About'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
 end
-
+ 
 
